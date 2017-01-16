75f6b955607d576329c3cef400bb5111e4c93c1b
Alexis Renaux <imareek26@gmail.com>
2017-01-16 11:46:37 +0100

    commencement AfficheGrille

--- p_virus.adb
+++ p_virus.adb
@@ -11,4 +11,13 @@ package p_virus is
     end loop;
   end AfficheVectVirus;
 
+procedure AfficheGrille (V : in TV_Virus) is
+-- {} => {Les valeurs du vecteur V sont affichees sur une ligne}
+
+begin --AfficheGrille
+  ecrire("");
+end AfficheGrille;
+
+
+
 end p_virus;
