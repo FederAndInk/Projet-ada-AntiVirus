cf20198f70eb63b9561839df7ebda4214131dcab
Ludovic Jozeau <federandink@gmail.com>
2017-01-16 11:42:14 +0100

    test creation du vecteur

--- p_virus.adb
+++ p_virus.adb
@@ -1,5 +1,10 @@
 package p_virus is
 
+procedure CreeVectVirus (f : in out file_type; nb : in integer; V :out TV_Virus);
+-- {f (ouvert) contient des configurations initiales,
+-- toutes les configurations se terminent par la position du virus rouge}
+-- => {V a ete initialise par lecture dans f de la partie de numero nb}
+
 
   procedure AfficheVectVirus (V : in TV_Virus) is
   -- {} => {Les valeurs du vecteur V sont affichees sur une ligne}
