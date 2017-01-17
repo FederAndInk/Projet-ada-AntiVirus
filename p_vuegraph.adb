b8dfcd5ac88e0f2dba4bde6a405f7f736ae8b6fc
federandink <federandink@gmail.com>
2017-01-18 00:45:22 +0100

    possible rajout + convertPiece
    Possible : tout les cas sont traités
    convertPiece : converti les couleurs T_piece en couleurs T_couleur (FL_...)
    ajout de regle1/2 pour le tuto
    fenetre bien mise et belle
    tuto complet !

--- p_vuegraph.adb
+++ p_vuegraph.adb
@@ -51,6 +51,7 @@ begin --LancerPartie
         partie:=(i-1)*5+J; --on donne la valeur du bouton :)
         ChangerTexte(Fpartie, "Info", "Partie selectionnee : " & integer'image(partie));
       end if;
+
       if Bouton="BoutonQuitter" then
         CacherFenetre(Fpartie);
         continuer:=false;
@@ -67,6 +68,31 @@ begin --LancerPartie
 
 end LancerPartie;
 
+------------------------------------------------------
+
+function convertPiece(piece : in t_piece) return T_Couleur is
+--{} => {renvoit la couleur T_Couleur correspondante à la couleur de t_piece}
+
+begin --convertPiece
+  case piece is
+    when rouge => return FL_RED;
+    when turquoise => return FL_CYAN;
+    when orange => return FL_DARKORANGE;
+    when rose => return FL_MAGENTA;
+    when marron => return FL_DARKTOMATO;
+    when bleu => return FL_BLUE;
+    when violet => return FL_DARKVIOLET;
+    when vert => return FL_GREEN;
+    when jaune => return FL_YELLOW;
+    when blanc => return FL_WHITE;
+    when vide => return FL_INACTIVE;
+  end case;
+end convertPiece;
+
+
+------------------------------------------------------
+
+
 procedure MajAffichage(v : in TV_Virus; Fen : in out TR_Fenetre) is
 --{} => {}
 j:character;
@@ -81,7 +107,7 @@ begin --MajAffichage
     end if;
     while j <= v'last(2) loop
       nombouton := Integer'Image(I)(2..2) & J;
-      ChangerCouleurFond(Fen,nombouton,FL_red);
+      ChangerCouleurFond(Fen,nombouton,convertPiece(v(i,j)));
       --v(i,j)));
       if j/= T_col'succ(v'last(2)) then
         j := T_col'succ(j);
@@ -91,11 +117,14 @@ begin --MajAffichage
       end if;
     end loop;
   end loop;
-
 end MajAffichage;
 
+
+------------------------------------------------------
+
+
 procedure InitGrid(Fen : in out TR_Fenetre; name : in string; X, Y, cote, ecart:in natural) is
---{} => {a generer la grille de bouton sur la fenetre Fen, avec les attributs des boutons}
+--{} => {a generé la grille de bouton correspondant au plateau de jeu sur la fenetre Fen, avec les attributs des boutons}
 nombouton:string(1..2);
 j:character;
 jpos:integer;
@@ -123,27 +152,122 @@ begin --InitGrid
 end InitGrid;
 
 
+------------------------------------------------------
+
+procedure Regle1Block(v : in out TV_Virus; quitter: out Boolean) is
+  task Regle1;  --pour afficher une magnifique animation qui s'arrete lorsqu'on appuis sur quitter ou continuer
+  task body Regle1 is
+  begin
+    for i in 1..10 loop
+      delay 1.0;
+      Deplacement(v, violet, hg);
+      MajAffichage(v, FRegleJeu);
+      delay 1.0;
+      Deplacement(v, violet, bd);
+      MajAffichage(v, FRegleJeu);
+    end loop;
+  end Regle1;
+  ---------------------------------------------------
+begin
+  quitter:=false;
+  if Attendrebouton(FRegleJeu)="BoutonQuitter" then
+    CacherFenetre(FRegleJeu);
+    quitter:=true;
+  end if;
+  abort regle1; --permet d'arreter la tache pour continuer
+end Regle1Block;
+
+
+procedure Regle2Block(v:in out TV_Virus; quitter: out Boolean) is
+  task Regle2;
+  task body Regle2 is
+  begin
+    if Possible(v, violet, hg) then
+      Deplacement(v, violet, hg);
+      MajAffichage(v, FRegleJeu);
+    end if;
+    for i in 1..10 loop
+      delay 1.0;
+      Deplacement(v, rouge, hd);
+      MajAffichage(v, FRegleJeu);
+      delay 1.0;
+      Deplacement(v, rouge, hd);
+      MajAffichage(v, FRegleJeu);
+      delay 1.0;
+      Deplacement(v, rouge, bg);
+      MajAffichage(v, FRegleJeu);
+      delay 1.0;
+      Deplacement(v, rouge, bg);
+      MajAffichage(v, FRegleJeu);
+    end loop;
+  end Regle2;
+  begin
+    quitter:=false;
+    if Attendrebouton(FRegleJeu)="BoutonQuitter" then
+      CacherFenetre(FRegleJeu);
+      quitter:=true;
+    end if;
+    abort regle2;
+    while Possible(v, rouge, hd) loop
+      ecrire(Possible(v, rouge, hd));
+      Deplacement(v, rouge, hd);
+      MajAffichage(v, FRegleJeu);
+    end loop;
+end Regle2Block;
+
+
+
 procedure LancerRegleJeu(f:in out file_type) is
 --{f ouvert} => {a afficher les regles du jeu dans une fenetre}
   V:TV_Virus;
+  Q:boolean;
 begin
   reset(f, in_file);
 
-  FRegleJeu:=DebutFenetre("Regle du jeu",700,700);
-  AjouterBouton(FRegleJeu,"BoutonContinuer","Continuer",225,650,70,50);
-  AjouterBouton(FRegleJeu,"BoutonQuitter","Quitter",400,650,70,50); --(margeG, margeH, boutonL, boutonH)
-  AjouterTexte(FRegleJeu, "Info", "", 260, 20, 160, 20);
+  FRegleJeu:=DebutFenetre("Regle du jeu",550,700);
+  AjouterBouton(FRegleJeu,"BoutonContinuer","Continuer",145,650,70,50);
+  AjouterBouton(FRegleJeu,"BoutonQuitter","Quitter",320,650,70,50); --(margeG, margeH, boutonL, boutonH)
+  AjouterTexte(FRegleJeu, "Info","test", 170, 500, 200, 100);
   --AjouterBouton(FRegleJeu,nombouton,"",(jpos-1)*35+50,(I-1)*35+40,30,30); --la multiplication permet d'appliquer des ecarts entre les boutons et l'ajout, l'ecarts aux bords.
-  InitGrid(FRegleJeu, "", 50, 40, 30, 5);
+  InitGrid(FRegleJeu, "", 50, 40, 60, 5);
   FinFenetre(FRegleJeu);
   InitVect(v);
   CreeVectVirus(f,1, v);
+  MontrerFenetre(FRegleJeu);
   MajAffichage(v, FRegleJeu);
 
-  MontrerFenetre(FRegleJeu);
-  if Attendrebouton(FRegleJeu)/="BoutonQuitter" then
-    ecrire("test");
+  Changertexte(FRegleJeu, "Info", "Vous pouvez deplacer la piece" & NewLine & "de couleur que vous voulez" & NewLine & "en cliquant dessus puis en " & NewLine & "utilisant les fleches");
+  Regle1Block(v, q);
+
+  if not q then
+    Changertexte(FRegleJeu, "Info", "Vous vous deplacer en diagonale");
+    Regle2Block(v, q);
+    if not q then
+      Changertexte(FRegleJeu, "Info", "Pour enfin faire sortir le virus rouge" & NewLine & "en haut a gauche et c'est gueri !!");
+      while Possible(v, violet, bd) loop
+        ecrire(Possible(v, violet, bd));
+        Deplacement(v, violet, bd);
+        MajAffichage(v, FRegleJeu);
+      end loop;
+      Deplacement(v, rouge, bg);
+      Deplacement(v, rouge, hg);
+      Deplacement(v, rouge, bg);
+      Deplacement(v, rouge, hg);
+      MajAffichage(v, FRegleJeu);
+
+      if Attendrebouton(FRegleJeu)="BoutonContinuer" then
+        Changertexte(FRegleJeu, "Info", "Fin du tutoriel, Appuyez" & NewLine & "sur continuer pour jouer.");
+        if Attendrebouton(FRegleJeu)/="" then
+          Cacherfenetre(FRegleJeu);
+        end if;
+      else
+        Cacherfenetre(FRegleJeu);
+      end if;
+    end if;
   end if;
+  --if Attendrebouton(FRegleJeu)/="BoutonQuitter" then
+  --  ecrire("test");
+  --end if;
 end LancerRegleJeu;
 
 end p_vuegraph;
--- p_vuegraph.ads
+++ p_vuegraph.ads
@@ -5,12 +5,14 @@ with ada.calendar; use ada.calendar;
 with p_virus; use p_virus;
 with sequential_IO; use p_virus.p_Piece_IO;
 
+with Ada.Task_Identification;  use Ada.Task_Identification;
 
 package p_vuegraph is
-----------------------Instanciation
+----------------------Declarations
 
 --deux fenetres pour choisir la partie, puis jouer la partie.
 Fpartie, FJeu, FRegleJeu : TR_Fenetre;
+NewLine : constant Character := Character'Val (10);
 
 
 ---------------------Corps
@@ -21,14 +23,21 @@ procedure LancerPartie(partie:out integer; continuer : out boolean);
 procedure InitGrid(Fen : in out TR_Fenetre; name : in string; X, Y, cote, ecart:in natural);
 --{} => {a generer la grille de bouton sur la fenetre Fen, avec les attributs des boutons}
 
+procedure MajAffichage(v : in TV_Virus; Fen : in out TR_Fenetre);
+--{aucune case de v n'est vide} => {a mis a jour les couleurs des boutons de la fenetre Jeu en fonction de ce qu'il y a dans V}
+
+procedure Regle1Block(v : in out TV_Virus; quitter: out Boolean);
+--{} => {Pour le tuto, deplace la piece violette}
+
+procedure Regle2Block(v:in out TV_Virus; quitter: out Boolean);
+--{} => {Pour le tuto, deplace la piece rouge}
+
 procedure LancerRegleJeu(f:in out file_type);
 --{} => {a afficher les regles du jeu dans une fenetre}
 
 --procedure LancerJeu(...); --todo
 --{} => {a affiché la fenetre de jeu}
 
-procedure MajAffichage(v : in TV_Virus; Fen : in out TR_Fenetre);
---{aucune case de v n'est vide} => {a mis a jour les couleurs des boutons de la fenetre Jeu en fonction de ce qu'il y a dans V}
 
 
 
